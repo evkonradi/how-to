926a8bf7cee6fad0f302d0215b8c5b71f9c59e89
idonia <sidoniag@gmail.com>
2020-10-22 10:03:41 -0500

    add component placeholders

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import { ApolloProvider } from '@apollo/react-hooks';
 // import ApolloClient from 'apollo-boost';
 
 import Home from './pages/Home';
+import Nav from './components/Nav';
+import Footer from './components/Footer';
 
 // const client = new ApolloClient({
 //   request: (operation) => {
@@ -23,14 +25,17 @@ function App() {
   //  <ApolloProvider>
   //    <Router>
        <div>
+         <Nav />
          <Home></Home>
          {/* <StoreProvider> */}
-           {/* <Nav /> */}
+           
            {/* <Switch>
              <Route exact path="/" component={Home} /> */}
              {/* <Route component={NoMatch} /> */}
            {/* </Switch> */}
          {/* </StoreProvider> */}
+       
+       <Footer></Footer>
        </div>
   //    </Router>
   //  </ApolloProvider>
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,1 +1,12 @@
 //comment
+import React from 'react';
+
+const Nav = () => {
+    return(
+        <div>
+            <h5>this is where the nav goes</h5>
+        </div>
+    )
+};
+
+export default Nav;
