d11fdb17fdfdc2524c95121a6b6299af03f10818
ElK <62822011+evkonradi@users.noreply.github.com>
2020-10-25 11:56:14 -0500

    seed file is corrected - populates resources and users

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -27,16 +27,10 @@ db.once("open", async () => {
     Bake at 400° for 40-45 minutes or until crust is browned and filling is bubbly. Remove from the oven; arrange remaining berries over top. Cool on a wire rack. Store in the refrigerator.',
       images: [
         {
-          fileName: "blueberry.jpg",
-          fileURL: "../images/blueberry.jpg",
-          imageCaption: "blueberry tart",
-        },
-      ],
-      // videos: [{
-      //     fileName:'' ,
-      //     fileURL: '',
-      //     videoCaption: 'bake a tart',
-      // }],
+          fileURL: "/images/blueberry.jpg",
+          imageCaption: "blueberry tart"
+        }
+      ]
     },
 
     {
@@ -52,12 +46,11 @@ db.once("open", async () => {
     Combine the above ingredients together, pour into a spray bottle, shake, and then let infuse for a week before using.\nOnce done, you can use the natural solution to remove hard water stains, clean trash cans, wipe away wall smudges, and much more. Besides a fresh scent, the lemon rind may help boost cleaning power. Caution: Do not use acidic cleaners on granite, as they will etch the stone.",
       images: [
         {
-          fileName: "clean.jpg",
-          fileURL: "../images/clean.jpg",
-          imageCaption: "cleaning supplies",
-        },
+          fileURL: "/images/clean.jpg",
+          imageCaption: "cleaning supplies"
+        }
       ],
-      videos: [],
+      videos: []
     },
     {
       name: "Become a Star Gazer",
@@ -70,18 +63,20 @@ db.once("open", async () => {
       The first easy-to-spot star you encounter is Polaris, a triple-star system lying about 430 light-years away. It might surprise you how dim 2nd-magnitude Polaris looks — in fact, it’s the 50th-brightest star in the sky.",
       images: [
         {
-          fileName: ("polaris.jpg", "star.jpg"),
-          fileUrl: ("../images/polaris.jpg", "../images/star.jpg"),
-          imageCaption: "check out the stars",
+          fileURL: "/images/polaris.jpg",
+          imageCaption: "check out the stars"
         },
+        {
+          fileURL: "/images/stars.jpg",
+          imageCaption: "more on checking out the stars"
+        }
       ],
       videos: [
         {
-          fileName: 'a href="https://www.youtube.com/watch?v=XUbG8jboh4M"',
-          fileUrl: 'a href="https://www.youtube.com/watch?v=XUbG8jboh4M"',
-          videoCaption: "introduction to stargazing",
-        },
-      ],
+          fileURL: "https://v.ftcdn.net/01/98/10/59/240_F_198105905_bdZOHJrgMMZsvC8vGAo93SNaxv3kozvF_ST.mp4",
+          videoCaption: "introduction to stargazing"
+        }
+      ]
     },
     {
       name: "Knit a Hat",
@@ -95,18 +90,20 @@ db.once("open", async () => {
           But it will give you a cute, cozy hat that you can work up in no time.\n Below, you'll find everything you'll need to complete this hat, from video tutorials for the various techniques, tips and tricks for getting your first circular knitting project just right, and step-by-step photos, so you know you're on the right track!",
       images: [
         {
-          fileName: ("knit.jpeg", "knit_explain.jpeg"),
-          fileUrl: ("../images/knit.jpeg", "../images/knit_explain.jpeg"),
-          imageCaption: "knit a hat",
+          fileURL: "/images/knit.jpeg",
+          imageCaption: "1. Knit a hat"
         },
+        {
+          fileURL: "/images/knit_explain.jpeg",
+          imageCaption: "2. Knit a hat"
+        }
       ],
       videos: [
         {
-          fileName: 'a href="https://www.youtube.com/watch?v=szX6slq2Q8Y"',
-          fileUrl: 'a href="https://www.youtube.com/watch?v=szX6slq2Q8Y"',
-          videoCaption: "initial knitting techniques",
-        },
-      ],
+          fileURL: "https://www.youtube.com/watch?v=szX6slq2Q8Y",
+          videoCaption: "initial knitting techniques"
+        }
+      ]
     },
     {
       name: "Make Homemade Soda",
@@ -116,18 +113,20 @@ db.once("open", async () => {
         "Believe it or not, it’s actually super easy to make homemade soda.\n\ You only need a few basic ingredients, including your favorite fresh fruit.\n\ I love that, when you’re making homemade soda, you can control the sweetness. Now, you can enjoy a tall glass of refreshing fizzy bubbles without the artificial sugars, colors, and preservatives. Woot!\nThe key ingredients of these homemade soda recipes are (1) fresh fruit “syrup” and (2) seltzer water.\n\ We will be using fresh fruit and a simple syrup made from raw honey and water to sweeten the soda.  To make your honey simple syrup, just combine some raw honey with water and cook in a pan over medium-high heat until the honey is dissolved, and then set aside to cool.  Easy peasy.",
       images: [
         {
-          fileName: ("soda_pour.jpg", "lemons.jpeg"),
-          fileUrl: ("../images/soda_pour.jpg", "../images/lemons.jpeg"),
-          imageCaption: "make fresh soda",
+          fileURL: "/images/soda_pour.jpg",
+          imageCaption: "1. Make fresh soda"
         },
+        {
+          fileURL: "/images/lemons.jpeg",
+          imageCaption: "2. Make fresh soda"
+        }
       ],
       videos: [
         {
-          fileName: 'a href="https://www.youtube.com/watch?v=N1d9SH-2usw"',
-          fileUrl: 'a href="https://www.youtube.com/watch?v=N1d9SH-2usw"',
-          videoCaption: "making your own fresh soda",
-        },
-      ],
+          fileURL: "https://www.youtube.com/watch?v=N1d9SH-2usw",
+          videoCaption: "making your own fresh soda"
+        }
+      ]
     },
     {
       name: "Build a Children's Bookshelf",
@@ -138,18 +137,20 @@ db.once("open", async () => {
 Need a little inspiration? These DIY shelves are nothing short of genius.\n\ Best of all, many of them can be made from things you already have lying around the house! Turn a 5-gallon bucket into a contemporary storage cubby. Have some scrap wood and an old leather belt?\nBoom! You’ve got a chic, hanging shelf. The possibilities are endless!",
       images: [
         {
-          fileName: ("child_book.jpeg", "shelf.jpeg"),
-          fileUrl: ("../images/child_book.jpeg", "../images/shelf.jpeg"),
-          imageCaption: "easy steps to build a childrens bookshelf",
+          fileURL: "/images/child_book.jpeg",
+          imageCaption: "1. Easy steps to build a childrens bookshelf"
         },
+        {
+          fileURL: "/images/shelf.jpeg",
+          imageCaption: "2. Easy steps to build a childrens bookshelf"
+        }
       ],
       videos: [
         {
-          fileName: 'a href="https://www.youtube.com/watch?v=BfPOl4Rzdbw"',
-          fileUrl: 'a href="https://www.youtube.com/watch?v=BfPOl4Rzdbw"',
-          videoCaption: "learn to build a childrens bookshelf",
-        },
-      ],
+          fileURL: "https://www.youtube.com/watch?v=BfPOl4Rzdbw",
+          videoCaption: "learn to build a childrens bookshelf"
+        }
+      ]
     },
     {
       name: "Build a Spaceship",
@@ -160,18 +161,20 @@ Need a little inspiration? These DIY shelves are nothing short of genius.\n\ Bes
 Making a spaceship is pretty easy, I could drop a cube into my 3D model, and tell you it was an alien battle cruiser from the planet 'spot', and that would be that.\n Making a good spaceship is the hard part. Inventing a spacecraft that people really believe exists and has an identifiable purpose, and above all looks 'cool', is where your talent and artistry comes into play.",
       images: [
         {
-          fileName: ("tools.jpg", "ship_drawn.jpg"),
-          fileUrl: ("../images/tools.jpg", "../images/ship_drawn.jpg"),
-          imageCaption: "diy a spaceship",
+          fileURL: "/images/tools.jpg",
+          imageCaption: "1. Diy a spaceship"
         },
+        {
+          fileURL: "/images/ship_drawn.jpg",
+          imageCaption: "2. Diy a spaceship"
+        }
       ],
       videos: [
         {
-          fileName: 'a href="https://www.youtube.com/watch?v=IM7aeVa7Dpc"',
-          fileUrl: 'a href="https://www.youtube.com/watch?v=IM7aeVa7Dpc"',
-          videoCaption: "craft your own spaceship",
-        },
-      ],
+          fileURL: "https://www.youtube.com/watch?v=IM7aeVa7Dpc",
+          videoCaption: "craft your own spaceship"
+        }
+      ]
     },
     {
       name: "Make Homemade Babyfood",
@@ -180,18 +183,20 @@ Making a spaceship is pretty easy, I could drop a cube into my 3D model, and tel
         "As a mom to three kids, I know firsthand how the urge to make homemade baby food can be both exciting and a little daunting.\n And if you have a busy schedule or other kids in the house, finding the time to actually do it can seem impossible. But, there are so many healthy foods that you can transform into baby food purees with hardly any work or special equipment at all!",
       images: [
         {
-          fileName: ("baby_food.jpeg", "carrots.jpeg"),
-          fileUrl: ("../images/baby_food.jpeg", "../images/carrots.jpeg"),
-          imageCaption: "make fresh baby food at home",
+          fileURL: "/images/baby_food.jpeg",
+          imageCaption: "1. Make fresh baby food at home"
         },
+        {
+          fileURL: "/images/carrots.jpg",
+          imageCaption: "2. Make fresh baby food at home"
+        }
       ],
       videos: [
         {
-          fileName: 'a href="https://www.youtube.com/watch?v=0nFqrBIc8eg"',
-          fileUrl: 'a href="https://www.youtube.com/watch?v=0nFqrBIc8eg"',
-          videoCaption: "learn to make fresh baby food at home",
-        },
-      ],
+          fileURL: "https://www.youtube.com/watch?v=0nFqrBIc8eg",
+          videoCaption: "learn to make fresh baby food at home"
+        }
+      ]
     },
     {
       name: "Introduction to Ukulele",
@@ -200,18 +205,20 @@ Making a spaceship is pretty easy, I could drop a cube into my 3D model, and tel
         "So, you want to learn how to play a ukulele? Welcome! The first thing to notice is all the encouraging, happy people around you, cheering you on and helping you out.\n The ukulele is a social instrument, a song machine that magnetically draws people together to enjoy themselves. Be warned: The ukulele spirit is highly contagious.\n As soon as you master your first chords, strums, and songs you may find that you, too, are moved to share it with a friend.",
       images: [
         {
-          fileName: ("ukulele.jpeg", "chord_view.jpeg"),
-          fileUrl: ("../images/ukulele.jpeg", "../images/chord_view.jpeg"),
-          imageCaption: "learn to play a ukulele",
+          fileURL: "/images/ukulele.jpeg",
+          imageCaption: "1. Learn to play a ukulele"
+        },
+        {
+          fileURL: "/images/chord_view.jpg",
+          imageCaption: "2. Learn to play a ukulele"
         },
       ],
       videos: [
         {
-          fileName: 'a href="https://www.youtube.com/watch?v=MyRWQpXk8po"',
-          fileUrl: 'a href="https://www.youtube.com/watch?v=MyRWQpXk8po"',
-          videoCaption: "introduction to uklele song",
-        },
-      ],
+          fileURL: "https://www.youtube.com/watch?v=MyRWQpXk8po",
+          videoCaption: "introduction to uklele song"
+        }
+      ]
     },
     {
       name: "A Beginner Lesson to Drawing",
@@ -221,18 +228,20 @@ Making a spaceship is pretty easy, I could drop a cube into my 3D model, and tel
         "We all used to draw as kids. It was easy back then, no matter if you used color pencils, a stick, or your finger on a steamy window. But even then you might have noticed that some children drew better than you.\n\ Not that you knew what better meant—all you knew was the sweetness of praise. Whenever you heard someone else being praised, and your own work ignored, you felt worse and worse. Eventually, you gave up on drawing. Why would you continue if nobody cared?\nNow, whatever the reason, you want to come back, but it seems so scary. Those children who never gave up are working as professionals now, and their art looks almost magical. How could you possibly bridge that gap? Will you ever catch up on them?",
       images: [
         {
-          fileName: ("notepad.jpeg", "drawing.jpeg"),
-          fileUrl: ("../images/notepad.jpeg", "../images/drawing.jpeg"),
-          imageCaption: "learn to draw",
+          fileURL: "/images/notepad.jpeg",
+          imageCaption: "1. Learn to draw"
         },
+        {
+          fileURL: "/images/drawing.jpeg",
+          imageCaption: "2. Learn to draw"
+        }
       ],
       videos: [
         {
-          fileName: 'a href="https://www.youtube.com/watch?v=mio-_RyxOyQ"',
-          fileUrl: 'a href="https://www.youtube.com/watch?v=mio-_RyxOyQ"',
-          videoCaption: "drawing techniques for beginners",
-        },
-      ],
+          fileURL: "https://www.youtube.com/watch?v=mio-_RyxOyQ",
+          videoCaption: "drawing techniques for beginners"
+        }
+      ]
     },
   ]);
 
@@ -241,15 +250,17 @@ Making a spaceship is pretty easy, I could drop a cube into my 3D model, and tel
   await User.deleteMany();
 
   await User.create({
-    firstName: "Frank",
-    lastName: "Furter",
+    // firstName: "Frank",
+    // lastName: "Furter",
+    username: "ffurter",
     email: "frankf@testmail.com",
     password: "password12345",
   });
 
   await User.create({
-    firstName: "Hans",
-    lastName: "Solo",
+    // firstName: "Hans",
+    // lastName: "Solo",
+    username: "hsolo",
     email: "hansyboy@testmail.com",
     password: "theforce123",
   });
