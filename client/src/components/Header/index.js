45e1953f3b6807689f9418516cc229ac13834c4e
Pochobs <pochobs@hotmail.com>
2020-10-26 23:54:50 -0500

    good night

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import {Navbar, Nav, Search} from 'reactstrap'
+import {FaBookOpen} from 'react-icons/fa';
 export default
 class Header extends Component {
   render(){
   return (
     <div className="Header">
         <ul>
-            <li className="logoText">Teach <span className="meGreen ">Me</span>To</li>
+            <li className="logoText"> <FaBookOpen  className="text-center"/> Teach <span className="meGreen ">Me</span>To
+            
+            </li>
             <li><a href="/">Home</a></li>
             <li className="float-right"><a className="active" href="login">Sign Up | Log In</a></li>
             
