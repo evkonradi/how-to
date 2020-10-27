b8839d80508bfb05ccbd6f022568b884d2da1c66
Pochobs <pochobs@hotmail.com>
2020-10-26 21:50:09 -0500

    not yet

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
-
+import {Navbar, Nav, Search} from 'reactstrap'
 export default
 class Header extends Component {
   render(){
   return (
     <div className="Header">
-        <ul>
-            <li><a href="/">Home</a></li>
+        <ul>            
             <li className="logoText">Teach <span className="meGreen ">Me</span>To</li>
-            <li className="float-right"><a className="active" href="welcome">Sign Up / Log In</a></li>
-            
+            <li className="float-right"><a className="actived" href="login">Sign Up / Log In</a></li>
         </ul>
     </div>
   );
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,8 +1,6 @@
 import React, { Component} from 'react';
-import '../SignupLogin/index.css';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
-import {FacebookLoginButton, GoogleLoginButton} from 'react-social-login-buttons'
-import GoogleLogin from 'react-google-login'
+
 class Signup extends Component {
     render(){
     return (
@@ -24,24 +22,12 @@ class Signup extends Component {
                 <Input type="password" placeholder="Password"/>
             </FormGroup>
             <Button className="btn-lg btn-dark btn-block">Sign Up</Button>
-            <div className="text-center pt-3">
-                Or continue with your social account
-            </div>
-            <FacebookLoginButton className="mt-3 mb-3"/>
-            <GoogleLoginButton 
-                className="mt-3 mb-3" 
-                />
-            <GoogleLogin 
-            clientId="212266341985-bfblloh84d96u203r5i5b80l745ea911.apps.googleusercontent.com"
-            onSuccess={this.responseGoogle}
-            onFailure={this.responseGoogle}
-            cookiePolicy={'single_host_origin'}
-            />
+            
 
             <div className="text-center">
-                <a href="/sign-up">Sign Up</a>
+                <a href="/signup">Sign Up</a>
                 <span className="p-2">|</span>
-                <a href="/sign-up">Forgot Password</a>
+                <a href="/signup">Forgot Password</a>
                     
                     
             </div>
@@ -50,4 +36,3 @@ class Signup extends Component {
   }  
 }
 export default Signup;
-
