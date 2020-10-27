a3fced1882bca27b844cad0bd5f5f5fe590d0731
idonia <sidoniag@gmail.com>
2020-10-27 14:45:48 -0500

    divider added

--- client/src/pages/ProfilePage.js
+++ client/src/pages/ProfilePage.js
@@ -6,6 +6,7 @@ import { Col, Row, Container, Card, CardImg, CardText, CardBody, CardLink, CardH
 import { useParams } from 'react-router-dom';
 import ResourceAddEdit from './ResourceAddEdit';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Divider, Box, BoxProps } from "@chakra-ui/core";
 // import { idbPromise } from "../../utils/helpers";
 // import { useParams } from "react-router-dom";
 
@@ -18,26 +19,35 @@ const ProfilePage = () => {
       <div>
         
             <Container>
-              <Col sm="12" md="6" lg="8" offset="3">
+              {/* <Col sm="12" md="6" lg="8" offset="3"> */}
               <Row>
                 <h2>Welcome Back, UserName</h2>
               </Row>
-              <Row>
+              <Row xs="2">
+              <Col xs="4">
+              
                 <a href="/resource/:id?"><h5>Make a New Post</h5></a><br /><br />
-              </Row>
-              {/* <Switch>
-              <Route exact path="/resource/:id?" component={ResourceAddEdit} />
-                </Switch> */}
-              <Row>
+              
                 <p>User Stats</p>
-              </Row>
-              <Row>
+            
                 <p>Payment</p>
-              </Row>
-              <Row>
-                <h4>Edit User Info</h4>
-              </Row>
+           
+                <p>Edit User Info</p>
+           
+                </Col>
+              
+              <Divider color="black" orientation="vertical" />
+            
+             
+              
+              <Col xs="8">
+    
+                      <p>lets test the divider</p>
+                      
               </Col>
+              </Row>
+              
+              {/* </Col> */}
             </Container>
         
       </div>
