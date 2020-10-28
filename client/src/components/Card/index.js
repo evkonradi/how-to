04f4a0b47429be5491aba21daeb12b4671b2eb16
idonia <sidoniag@gmail.com>
2020-10-27 22:34:47 -0500

    cleaner components

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -1,4 +1,19 @@
-// this makes a really good looking card
+// this is more plain from 'react-bootstrap'
+{/* <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="holder.js/100px180" />
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card> */}
+
+
+// // this makes a really good looking card
+// https://material-ui.com/components/cards/
 
 
 
