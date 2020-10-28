fff9643d59b3d66462e59f6ec3b8f8d197057432
idonia <sidoniag@gmail.com>
2020-10-28 13:23:21 -0500

    better layout profile

--- client/src/pages/ProfilePage.js
+++ client/src/pages/ProfilePage.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React from "react";
 // import { Col, Row, Image, Container }  from 'reactstrap';
-import { useQuery } from '@apollo/react-hooks';
-import { QUERY_RESOURCE } from '../utils/queries';
-import { Col, Row, Container, Card, CardImg, CardText, CardBody, CardLink, CardHeader, CardTitle, CardSubtitle, Carousel, CarouselIndicators, CarouselControl, CarouselItem, CarouselCaption }  from 'reactstrap';
-import { useParams } from 'react-router-dom';
+import { useQuery } from "@apollo/react-hooks";
+import { QUERY_RESOURCE } from "../utils/queries";
+import {
+  Col,
+  Row,
+  Container,
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardLink,
+  CardHeader,
+  CardTitle,
+  CardSubtitle,
+} from "reactstrap";
+import { useParams } from "react-router-dom";
 // import ResourceAddEdit from './ResourceAddEdit';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Divider, Box, BoxProps } from "@chakra-ui/core";
@@ -13,47 +25,47 @@ import { Divider, Box, BoxProps } from "@chakra-ui/core";
 
 import { Button } from "@chakra-ui/core";
 
-
-
 const ProfilePage = () => {
-
   return (
     <main>
       <div>
-        
-            <Container>
-            <br />
-              <Col sm="12" md="6" lg="12" offset="3">
-              <Row>
+        <Container>
+          <br />
+          <Col sm="12" md="6" lg="12" offset="3">
+            <Row>
               <Box bg="#5C6B73" w="100%" p={4} color="#C2DFE3">
                 <h3>Welcome Back, UserName</h3>
-                </Box>
-                </Row>
+              </Box>
+            </Row>
+            <br />
+            <Row className="user" xs="4">
+              <p className="small">User Details</p>
+            </Row>
+            <Row>
+              <Col xs={3} md="3" lg="3">
+                <a href="/resource" className="small">
+                  New Post
+                </a>
                 <br />
-                <Row className="user" xs="4">
-                <p className="small">User Details</p>
-                </Row>
-              <Row xs="3">
-                <Col xs="3" md="3" lg="3">
-                <a href="/resource" className="small">New Post</a><br />
                 <p className="small">Wallet</p>
-                </Col> 
+              </Col>
               <Divider color="black" orientation="vertical" />
+              <Col xs={6}>
+                <Card>
+                  <CardHeader>This is where a Card will Go</CardHeader>
+                </Card>
+                <Card>
+                  <CardHeader>This is where a Card will Go</CardHeader>
+                </Card>
+                <Card>
+                  <CardHeader>This is where a Card will Go</CardHeader>
+                </Card>
+              </Col>
               </Row>
 
-              <Row xs="2">
-              <Col xs="6">
-                 
-    {/* <Resource></Resource> */}
-                      <p>this is where resources the user has created will go</p>
-                      
-              </Col>
-              </Row>
-              
-              
-              </Col>
-            </Container>
-        
+             
+          </Col>
+        </Container>
       </div>
     </main>
   );
