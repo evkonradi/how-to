7f52811c654b09743d3d62ad29a06a73bf6ea040
idonia <sidoniag@gmail.com>
2020-10-28 23:15:04 -0500

    something silly with header mobile

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
-import {Navbar, Nav, NavDropdown, NavDropdownProps, NavItem, NavItemProps, NavLink, NavLinkProps} from 'react-bootstrap'
+
+import { Row, Navbar, Nav, NavDropdown } from 'react-bootstrap'
+
 export default
 class Header extends Component {
   render(){
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="light">
+    <Navbar className="mainNav" collapseOnSelect expand="lg" bg="dark" variant="light">
     <Navbar.Brand href="/"><img className="small" src="./images/teach_me_to_logo-01.png"/></Navbar.Brand>
     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
     <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="mr-auto">
-      {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown"> */}
       <NavDropdown.Item href="/login">Login</NavDropdown.Item>
       <NavDropdown.Item href="/signup">Signup</NavDropdown.Item>
       <NavDropdown.Divider />
-    {/* </NavDropdown> */}
     </Nav>
     </Navbar.Collapse>
 </Navbar>
