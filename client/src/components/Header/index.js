de6d1ed1f12350bcfccf3de3bb82366f14382a30
Jenniffer <jenlpac@yahoo.com>
2020-10-29 13:50:26 -0500

    update navbar with logout option on profile pg

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,22 +1,43 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import { Row, Navbar, Nav, NavDropdown } from 'react-bootstrap'
+import Auth from '../../utils/auth';
 
-export default
-class Header extends Component {
-  render(){
+// export default
+const Header = (Component) => {
+  const logout = event => {
+    event.preventDefault();
+    Auth.logout();
+  };
+
+  // render(){
   return (
     <Navbar className="mainNav" collapseOnSelect expand="lg" bg="dark" variant="light">
     <Navbar.Brand href="/"><img className="small" src="./images/teach_me_to_logo-01.png"/></Navbar.Brand>
     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
     <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="mr-auto">
-      <NavDropdown.Item href="/login">Login</NavDropdown.Item>
-      <NavDropdown.Item href="/signup">Signup</NavDropdown.Item>
-      <NavDropdown.Divider />
+      {Auth.loggedIn() ? (
+        <>
+          <NavDropdown.Item>
+            <a href="/" onClick={logout} color="#9DB4C0">
+            Logout
+            </a>
+          </NavDropdown.Item>
+        </>
+      ) : (
+        <>
+          <NavDropdown.Item href="/login">Login</NavDropdown.Item>
+          <NavDropdown.Item href="/signup">Signup</NavDropdown.Item>
+          <NavDropdown.Divider />
+        </>
+      )}
     </Nav>
     </Navbar.Collapse>
-</Navbar>
+    </Navbar>
   );
+// }
 }
-}
+
+export default Header;
