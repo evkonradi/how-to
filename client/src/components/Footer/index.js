e9fd7b78253f331a3472100dc2bc1ce8d8602453
Pochobs <pochobs@hotmail.com>
2020-10-29 18:05:59 -0500

    deleted files

--- client/src/components/Footer/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Footer = () => (
-  <div className="footer">
-    <p>teachmeto© 2020 <a href="https://github.com/evkonradi/teach-me-to"></a></p>
-  </div>
-);
-
-export default Footer;
