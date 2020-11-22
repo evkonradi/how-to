bc3903f1b5ade1efa19eed614d61223e9ac8b52f
idonia <sidoniag@gmail.com>
2020-11-21 23:12:57 -0600

    updated home rough

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -18,14 +18,7 @@ const Home = () => {
             <Col id="col2" sm={{size:8, offset:2}} lg={{ size:6, offset:3}} className="carousel cardTextAlign" >
                 <Resource></Resource>
             </Col> 
-            <Jumbotron className="hometron">
-                <Row xs={1} md={2}>
-                    <Col xs={{ size: 12 }} sm={{ size: 6, order: 2, offset: 1}} text="center">
-                    <h1>Teach Me To.</h1>
-                    <h3>What do you want to learn today?</h3>
-                    </Col>
-                </Row>
-            </Jumbotron>
+ 
             <Jumbotron className="whitespace" />
             <br />
             
