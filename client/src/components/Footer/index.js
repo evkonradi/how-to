499e9dab828466d6d131ea6de20b5c50cc70a9a1
idonia <sidoniag@gmail.com>
2020-11-28 22:17:46 -0600

    start cart style

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Cart from "../Cart";
 
 const Footer = () => (
   <div className="footer">
+    <Cart></Cart>
     <div>
       <span className="footerP">teachmeto© 2020</span>  
     </div>
+    
     <div>
       <a class="nullA" href="https://github.com/evkonradi/teach-me-to">Proudly made in Texas</a>
     </div>
