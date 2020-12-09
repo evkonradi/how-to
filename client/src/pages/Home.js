4c597b7c7826508840ebacfa518c3da4d7e02831
idonia <sidoniag@gmail.com>
2020-12-08 22:17:55 -0600

    cleaner home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -9,17 +9,15 @@ const Home = () => {
         <div >
             
             <Jumbotron className="hometron">
-                
-                    <Col xs={{ size: 12 }} sm={{ size: 12, order: 2, offset: 1}} text="center">
+                    <Col xs={{ size: 12 }} sm={{ size: 12, offset: 1}} text="center">
                     <h1>Teach Me To.</h1>
                     <h3 className="byline">What do you want to learn today?</h3>
                     </Col>
-               
             </Jumbotron>
             
             <Col className="homesearch" sm={{size:8, offset:2}} lg={{ size:6, offset:3}}>
                 <Search></Search>
-            </Col><br />
+            </Col>
 
             <p className="recentlyAddP">Recently added articles</p>
             <Col id="col2" sm={{size:8, offset:2}} lg={{ size:6, offset:3}} className="carousel cardTextAlign" >
