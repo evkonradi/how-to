8e02eb12951229a6669496fbc7a5362673e0ec0a
idonia <sidoniag@gmail.com>
2020-12-12 21:50:15 -0600

    need new nav

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -47,3 +47,33 @@ const Header = (Component) => {
 }
 
 export default Header;
+
+// import React, { useState } from 'react';
+// import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+
+// const Example = (props) => {
+//   const [collapsed, setCollapsed] = useState(true);
+
+//   const toggleNavbar = () => setCollapsed(!collapsed);
+
+//   return (
+//     <div>
+//       <Navbar color="faded" light>
+//         <NavbarBrand href="/" className="mr-auto">reactstrap</NavbarBrand>
+//         <NavbarToggler onClick={toggleNavbar} className="mr-2" />
+//         <Collapse isOpen={!collapsed} navbar>
+//           <Nav navbar>
+//             <NavItem>
+//               <NavLink href="/Login">Login</NavLink>
+//             </NavItem>
+//             <NavItem>
+//               <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+//             </NavItem>
+//           </Nav>
+//         </Collapse>
+//       </Navbar>
+//     </div>
+//   );
+// }
+
+// export default Header;
