349971a99e9337c55ccda7d56430a326c7f997d5
Jenniffer <jenlpac@yahoo.com>
2020-12-14 21:04:36 -0600

    update resource bug squashed

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -106,7 +106,7 @@ const typeDefs = gql`
     login(email: String!, password: String!): Auth
     addUser(username: String!, firstName: String!, lastName: String!, displayName: String!, email: String!, password: String!): Auth
     addResource(name: String!, shortDescription: String!, resourceBody: String, cost: String, images: [ImageInput], videos: [VideoInput]): Resource
-    updateResource(_id: ID!, name: String!, shortDescription: String!, resourceBody: String, cost: String!, images: [ImageInput], videos: [VideoInput]): Resource
+    updateResource(_id: ID!, name: String!, shortDescription: String!, resourceBody: String!, cost: String!, images: [ImageInput], videos: [VideoInput]): Resource
     deleteResource(_id: ID!): Resource
     updateWallet(username: String!, amount: Float, resource_id: ID, resource_name: String): User
   }
