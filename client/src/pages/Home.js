d044fc26f8f179be3d6346bae76cca96113b54d4
idonia <sidoniag@gmail.com>
2021-01-06 17:36:00 -0600

    merge new style

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,97 +1,53 @@
-import React from 'react';
-import { Jumbotron, Col} from 'reactstrap';
+import React from "react";
+import { Jumbotron, Col } from "reactstrap";
 import Resource from "../components/Resource";
-import Search from '../components/Search';
+import Search from "../components/Search";
 import logo from "./teach_me_to_logo-01.png";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { Box, Flex } from "@chakra-ui/core";
 import graphic from "./AdobeStock_276150433_graphic.png";
 
-// const Home = () => {
-//     return (
-//         <div >
-     
-//             <Box className="hometron">
-            
-//             <img className="medium" src={logo} alt="logo, teach me to" />
-//                     <Col className="heading"  text="center">
-//                     <h1>Teach Me To.</h1>
-//                     <h4 className="byline">What do you want to learn today?</h4>
-//                     </Col>
-//                     <Col className="homesearch" sm={{size:8, offset:2}} lg={{ size:6, offset:3}} bg={$primary}>
-//                 <Search></Search>
-//             </Col>
-//             </Box>
-            
-//             {/* <Box bg="transparent" h="30px"></Box> */}
-
-//             <Box id="col2" sm={{size:8, offset:2}} lg={{ size:6, offset:3}} className="carouselMain" >
-//             {/* <Box className="badgeNew">NEW POST</Box> */}
-//             <p className="new">NEW POSTS</p>
-//                 <Resource></Resource>
-//             </Box> 
-
-           
-//             <Jumbotron className="whitespace" align="center" bg={$primary}>
-//             <Box className="copyBox" maxW="50%">
-//             <p className="copy"> Start creating and sharing your knowledge with others on TeachMeTo.</p> 
-//             <p className="copy">Add images, video, and the "written" word.</p>
-//             <Link className="copyLink" to="/signup">Signup |</Link>
-//             <Link className="copyLink" to="/login">Login</Link>
-//             </Box>
-//             </Jumbotron>
-
-
-//         </div>
-//     );
-// };
 
 const Home = () => {
-    return (
-        <div className="main-container">
-            
+  return (
+    <div className="main-container">
+      <div className="main-grid">
+        <Box bg="transparent" height="40px"></Box>
+        {/* <Box className="main-grid-item" > */}
+        <Box className="copyBox">
+          <img className="medium" src={logo} alt="logo, teach me to" />
+          <h1 className="mainTitle">TeachMeTo</h1>
+          <h4 className="byline">WHAT DO YOU WANT TO LEARN TODAY?</h4>
 
-            <div className="main-grid">
-           <Box bg="transparent" height="40px"></Box>
-                    {/* <Box className="main-grid-item" > */}
-                    <Box className="copyBox">
-                    <img className="medium" src={logo} alt="logo, teach me to" />
-                    <h1 className="mainTitle">TeachMeTo</h1>
-                    <h4 className="byline">WHAT DO YOU WANT TO LEARN TODAY?</h4>
-                    
-                    <Search></Search>
-                
-                    <img className="graphic" src={graphic}/>
-            <p className="copy"> Start creating and sharing your knowledge with others on TeachMeTo.<br/>
-Add images - videos - & words to help others learn.<br/> Share FREE or $$$ resources today.</p>
-            <Link className="copyLink" to="/signup">Signup |</Link>
-            <Link className="copyLink" to="/login">Login</Link>
-   
-            
-            </Box>
-            <Box className="copyBox" >
-            <h4 className="byline">SEE WHAT'S NEW</h4>
-                <Flex display="center">
-                <Resource></Resource>
-                </Flex>
-                </Box>   
-            </div>
-                    {/* <Col className="homesearch" sm={{size:8, offset:2}} lg={{ size:6, offset:3}} bg={$primary}>
-                <Search></Search>
-            </Col> */}
+          <Search></Search>
 
-            
-            {/* <Box bg="transparent" h="30px"></Box> */}
-           
-            {/* <Box id="col2" sm={{size:8, offset:2}} lg={{ size:6, offset:3}} className="carouselMain" > */}
-            {/* <Box className="badgeNew">NEW POST</Box> */}
-         
-           
-          
-       
-       
-        </div>
-    );
+          <img className="graphic" src={graphic} />
+          <p className="copy">
+            {" "}
+            Start creating and sharing your knowledge with others on TeachMeTo.
+            <br />
+            Add images - videos - & words to help others learn.
+            <br /> Share FREE or $$$ resources today.
+          </p>
+          <Link className="copyLink" to="/signup">
+            Signup |
+          </Link>
+          <Link className="copyLink" to="/login">
+            Login
+          </Link>
+        </Box>
+        <Box bg="transparent" height="40px" />
+
+        <Box className="copyBox">
+          <Box bg="transparent" height="40px" />
+          <h4 className="byline">SEE WHAT'S NEW</h4>
+          <Flex display="center">
+            <Resource></Resource>
+          </Flex>
+        </Box>
+      </div>
+    </div>
+  );
 };
 
-    export default Home; 
+export default Home;
