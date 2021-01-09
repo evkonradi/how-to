9e1da60fecc89ff28266d9346b315ca8ef3041d1
idonia <sidoniag@gmail.com>
2021-01-09 14:38:01 -0600

    card on large scrn

--- client/src/components/CardResource/index.js
+++ client/src/components/CardResource/index.js
@@ -7,13 +7,11 @@ const CardResource = ({resource, useClass}) =>{
 
   return (
 
-
-    <Link className="mainCard" to={`/articles/${resource._id}`} key={resource._id}>
-      <Box height="250px" width="500px">
-      
+    
       <Card outline color='dark'>
-     
-        <Card.Body className="cardBody">
+        <Link className="mainCard" to={`/articles/${resource._id}`} key={resource._id}>
+
+        <Card.Body >
           <img className={`cardImg ${useClass}`} variant="top" src={resource.images.length ? resource.images[0].fileURL : "/images/teach_me_to_logo-01.png"} alt = {resource.name} />
           <Box className="cardFill">
           <Card.Title className="cardTitle">{resource.name}</Card.Title>
@@ -21,12 +19,9 @@ const CardResource = ({resource, useClass}) =>{
           <Card.Text className="cardTime">Created: {resource.dateCreated} by {resource.displayName}</Card.Text>
           </Box>
         </Card.Body>
-  
-      </Card>
- 
-      </Box>
+   
     </Link>
-
+    </Card>
 );
 };
 
